512efe75a5d87e27e2c5649371c61c6da913a259
prryplatypus <25409753+prryplatypus@users.noreply.github.com>
2023-06-06 22:51:33 +0000

    Deploying to gh-pages from @ prryplatypus/prryplatypus.dev@48735717a2b0f2851d7576e6e83b9db466a85010 🚀

--- _app/immutable/chunks/stores.06e53b69.js
+++
@@ -1,1 +0,0 @@
-import"./index.05ca2464.js";import{s as e}from"./singletons.466b77b9.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
