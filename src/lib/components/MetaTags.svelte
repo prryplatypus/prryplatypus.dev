916316109be0543c9c55793e35cb16b3d27bf21d
prryplatypus <25409753+prryplatypus@users.noreply.github.com>
2023-06-07 01:11:28 +0200

    Try without card type

--- src/lib/components/MetaTags.svelte
+++ src/lib/components/MetaTags.svelte
@@ -1,23 +1,26 @@
 <script>
     export let title = "";
     export let description = "";
-    export let image = "/apple-touch-icon.png";
     export let domain = "prryplatypus.dev";
+    export let image = "/apple-touch-icon.png";
+
+    let url = `https://${domain}`;
+    let image_ = `${url}/${image}`;
 </script>
 
 <title>{title}</title>
 <meta name="description" content={description} />
 
 <!-- Facebook Meta Tags -->
-<meta property="og:url" content="https://{domain}" />
+<meta property="og:url" content={url} />
 <meta property="og:type" content="website" />
 <meta property="og:title" content={title} />
 <meta property="og:description" content={description} />
-<meta property="og:image" content={image} />
+<meta property="og:image" content={image_} />
 
 <!-- Twitter Meta Tags -->
-<meta name="twitter:card" content="summary_large_image" />
 <meta property="twitter:domain" content={domain} />
-<meta property="twitter:url" content="https://{domain}" />
+<meta property="twitter:url" content={url} />
 <meta name="twitter:title" content={title} />
 <meta name="twitter:description" content={description} />
+<meta name="twitter:image" content={image_} />
