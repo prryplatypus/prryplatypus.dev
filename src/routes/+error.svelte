b22e6c29ccda878cacfbd5ddeb7dacdc5bb44551
prryplatypus <25409753+prryplatypus@users.noreply.github.com>
2023-06-06 21:31:20 +0200

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
+<svelte:head>
+    <title>{$page.status} - {$page.error.message}</title>
+    <meta name="description" content="{$page.status} - {$page.error.message}" />
+</svelte:head>
+
+<div>
+    <h1>{$page.status}</h1>
+    <h3>{$page.error.message}</h3>
+</div>
+
+<style>
+    div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        min-height: 100vh;
+    }
+
+    h1 {
+        margin-bottom: 0;
+        padding-bottom: 0;
+    }
+
+    h3 {
+        margin-top: 0;
+        padding-top: 0;
+    }
+</style>
