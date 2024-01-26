9df4468ef82f0f2ef497bdce44bfc86cd13786a7
prryplatypus <25409753+prryplatypus@users.noreply.github.com>
2024-01-26 05:51:52 +0000

    Deploying to gh-pages from @ prryplatypus/prryplatypus.dev@43b63837add4788e22c854246505c61fe736992b 🚀

---
+++ _app/immutable/nodes/2.4437a100.js
@@ -0,0 +1,1 @@
+import{L as n}from"../chunks/_layout.473af3f4.js";const e=!0,o=Object.freeze(Object.defineProperty({__proto__:null,prerender:e},Symbol.toStringTag,{value:"Module"}));export{n as component,o as universal};
---
+++ _app/immutable/nodes/4.7df0d78f.js
@@ -0,0 +1,101 @@
+import{S as Sa,i as ka,s as qa,k as a,q as r,a as i,l as s,m as n,r as l,h as t,c,n as d,b as ct,H as e,I as pe,u as fs,o as gs,y as _a,z as Ea,A as wa,g as ba,d as Ia,B as Pa}from"../chunks/index.93dd95e1.js";import{P as ys}from"../chunks/PageHead.f30ff5e5.js";function _s(P){let h,u,m,p,v,E,o,g,w,M,f,y,I,Z,S,dt,ht,ve,ut,mt,fe,pt,vt,ge,ft,gt,k,ye,yt,_t,_,q,_e,Et,wt,Ee,z,A,K,bt,It,Q,Pt,St,we,kt,qt,H,be,At,Ht,Ie,U,C,X,Ct,$t,ee,Lt,Nt,Pe,Dt,Ft,$,Se,Ot,Tt,ke,R,L,te,xt,jt,ae,Vt,Mt,qe,zt,Ut,N,Ae,Rt,Wt,W,J,D,se,Jt,Bt,ne,Yt,Gt,He,Zt,Kt,B,F,oe,Qt,Xt,re,ea,ta,Ce,aa,sa,O,$e,na,oa,Le,Y,T,le,ra,la,ie,ia,ca,Ne,da,ha,x,De,ua,ma,ce,j,Fe,pa,va,Oe,G,de,he,fa,ga,Te,ya;return{c(){h=a("main"),u=a("div"),m=a("article"),p=a("h2"),v=r("About me"),E=i(),o=a("div"),g=a("p"),w=r(`Hi there, nice to meet you! I'm a software engineer who's
+                    currently working in network automation. Here's a quick
+                    overview about me:`),M=i(),f=a("ul"),y=a("li"),I=r(`Passion for open-source. Have contributed in several
+                        occassions to different OSS projects; mainly those in
+                        the Python community.`),Z=i(),S=a("li"),dt=r(`Always advocate for following well-known design
+                        principles (SOLID, DRY, YAGNI, etc...). Writing clean
+                        code guarantees not only that it's easy to read, but
+                        also that it's easy to maintain, expand and test.`),ht=i(),ve=a("li"),ut=r(`Iterating over and over again is essential. Nobody gets
+                        things perfect on the first try. Code is something that
+                        should be "alive".`),mt=i(),fe=a("p"),pt=r(`In general, I excel in any type of coding with Python, but I
+                    have the most experience with async frameworks such as
+                    Sanic, FastAPI or discord.py.`),vt=i(),ge=a("p"),ft=r(`Please reach out if you have work opportunities, want to
+                    collaborate, or just want to say hi!`),gt=i(),k=a("article"),ye=a("h2"),yt=r("Experience"),_t=i(),_=a("div"),q=a("section"),_e=a("h3"),Et=r("Ziply Fiber"),wt=i(),Ee=a("ul"),z=a("li"),A=a("span"),K=a("h4"),bt=r("Software Engineer"),It=i(),Q=a("p"),Pt=r("July 2023 - Present"),St=i(),we=a("p"),kt=r(`Part of the network automation team. Working on
+                                internal APIs and tools to automate network
+                                operations.`),qt=i(),H=a("section"),be=a("h3"),At=r("PacketFabric"),Ht=i(),Ie=a("ul"),U=a("li"),C=a("span"),X=a("h4"),Ct=r("Junior Software Engineer"),$t=i(),ee=a("p"),Lt=r("September 2022 - May 2023"),Nt=i(),Pe=a("p"),Dt=r(`Part of the core development team. Worked on
+                                network-oriented APIs and all the shenanigans
+                                that make it work, like asynchronous task
+                                workers, message queues, and data storage.
+                                Additionally, I reworked the CI / CD flows and
+                                the image build and deployment processes.
+                                Provided customer-centric support with any
+                                development-related issues.`),Ft=i(),$=a("section"),Se=a("h3"),Ot=r("SilentForce"),Tt=i(),ke=a("ul"),R=a("li"),L=a("span"),te=a("h4"),xt=r("Security Engineering Intern"),jt=i(),ae=a("p"),Vt=r("December 2021 - June 2022"),Mt=i(),qe=a("p"),zt=r(`Performed offensive security testing of web and
+                                mobile applications. Helped with the development
+                                of a tool that allowed continuous monitoring,
+                                discovery and testing of customer assets in an
+                                automated fashion. Other notable projects are
+                                the development of an API that exposed all
+                                gathered data to the customers and an
+                                asynchronous web spider that crawled customer
+                                web services in search of sensitive data that
+                                could be used by attackers.`),Ut=i(),N=a("section"),Ae=a("h3"),Rt=r("Sanic Framework (open-source, volunteering)"),Wt=i(),W=a("ul"),J=a("li"),D=a("span"),se=a("h4"),Jt=r("Steering Council"),Bt=i(),ne=a("p"),Yt=r("October 2022 - Present"),Gt=i(),He=a("p"),Zt=r(`Ensured the smooth operation of the community
+                                projects by removing impediments, and assisting
+                                members as needed. Voiced any concerns,
+                                suggestions or ideas. Handled RFCs, enforced the
+                                community's code of conduct and administered
+                                community assets.`),Kt=i(),B=a("li"),F=a("span"),oe=a("h4"),Qt=r("Core Developer"),Xt=i(),re=a("p"),ea=r("October 2021 - October 2022"),ta=i(),Ce=a("p"),aa=r(`Submitted various contributions to the project,
+                                assisted the community and handled bug reports.
+                                Contributed to various discussions about the
+                                long-term direction of the project.`),sa=i(),O=a("section"),$e=a("h3"),na=r("VATSIM Network (non-profit, volunteering)"),oa=i(),Le=a("ul"),Y=a("li"),T=a("span"),le=a("h4"),ra=r("Software Developer"),la=i(),ie=a("p"),ia=r("March 2019 - September 2021"),ca=i(),Ne=a("p"),da=r(`Contributed to the development of several of
+                                services; most notably to their proprietary
+                                audio simulation codec known as "Audio For
+                                VATSIM (AFV)". This codec is used by all of
+                                their clients and simulates real-life VHF and HF
+                                radio transmissions, decreasing audio quality as
+                                distance between transmitter and receiver
+                                increases in the simulation.`),ha=i(),x=a("article"),De=a("h2"),ua=r("Education"),ma=i(),ce=a("div"),j=a("section"),Fe=a("h3"),pa=r(`U-tad | Centro Universitario de Tecnología y Arte
+                        Digital`),va=i(),Oe=a("ul"),G=a("li"),de=a("span"),he=a("h4"),fa=r(`Bachelor in Software Engineering
+                                    (Cybersecurity specialization)`),ga=i(),Te=a("p"),ya=r(`Honors in Introduction to Programming I & II,
+                                Introduction to Cybersecurity, Distributed
+                                Systems Programming and Networking subjects.`),this.h()},l(xe){h=s(xe,"MAIN",{class:!0});var je=n(h);u=s(je,"DIV",{class:!0});var ue=n(u);m=s(ue,"ARTICLE",{class:!0});var Ve=n(m);p=s(Ve,"H2",{});var Aa=n(p);v=l(Aa,"About me"),Aa.forEach(t),E=c(Ve),o=s(Ve,"DIV",{class:!0});var V=n(o);g=s(V,"P",{});var Ha=n(g);w=l(Ha,`Hi there, nice to meet you! I'm a software engineer who's
+                    currently working in network automation. Here's a quick
+                    overview about me:`),Ha.forEach(t),M=c(V),f=s(V,"UL",{});var me=n(f);y=s(me,"LI",{});var Ca=n(y);I=l(Ca,`Passion for open-source. Have contributed in several
+                        occassions to different OSS projects; mainly those in
+                        the Python community.`),Ca.forEach(t),Z=c(me),S=s(me,"LI",{});var $a=n(S);dt=l($a,`Always advocate for following well-known design
+                        principles (SOLID, DRY, YAGNI, etc...). Writing clean
+                        code guarantees not only that it's easy to read, but
+                        also that it's easy to maintain, expand and test.`),$a.forEach(t),ht=c(me),ve=s(me,"LI",{});var La=n(ve);ut=l(La,`Iterating over and over again is essential. Nobody gets
+                        things perfect on the first try. Code is something that
+                        should be "alive".`),La.forEach(t),me.forEach(t),mt=c(V),fe=s(V,"P",{});var Na=n(fe);pt=l(Na,`In general, I excel in any type of coding with Python, but I
+                    have the most experience with async frameworks such as
+                    Sanic, FastAPI or discord.py.`),Na.forEach(t),vt=c(V),ge=s(V,"P",{});var Da=n(ge);ft=l(Da,`Please reach out if you have work opportunities, want to
+                    collaborate, or just want to say hi!`),Da.forEach(t),V.forEach(t),Ve.forEach(t),gt=c(ue),k=s(ue,"ARTICLE",{class:!0});var Me=n(k);ye=s(Me,"H2",{});var Fa=n(ye);yt=l(Fa,"Experience"),Fa.forEach(t),_t=c(Me),_=s(Me,"DIV",{class:!0});var b=n(_);q=s(b,"SECTION",{class:!0});var ze=n(q);_e=s(ze,"H3",{});var Oa=n(_e);Et=l(Oa,"Ziply Fiber"),Oa.forEach(t),wt=c(ze),Ee=s(ze,"UL",{});var Ta=n(Ee);z=s(Ta,"LI",{});var Ue=n(z);A=s(Ue,"SPAN",{class:!0});var Re=n(A);K=s(Re,"H4",{class:!0});var xa=n(K);bt=l(xa,"Software Engineer"),xa.forEach(t),It=c(Re),Q=s(Re,"P",{class:!0});var ja=n(Q);Pt=l(ja,"July 2023 - Present"),ja.forEach(t),Re.forEach(t),St=c(Ue),we=s(Ue,"P",{});var Va=n(we);kt=l(Va,`Part of the network automation team. Working on
+                                internal APIs and tools to automate network
+                                operations.`),Va.forEach(t),Ue.forEach(t),Ta.forEach(t),ze.forEach(t),qt=c(b),H=s(b,"SECTION",{class:!0});var We=n(H);be=s(We,"H3",{});var Ma=n(be);At=l(Ma,"PacketFabric"),Ma.forEach(t),Ht=c(We),Ie=s(We,"UL",{});var za=n(Ie);U=s(za,"LI",{});var Je=n(U);C=s(Je,"SPAN",{class:!0});var Be=n(C);X=s(Be,"H4",{class:!0});var Ua=n(X);Ct=l(Ua,"Junior Software Engineer"),Ua.forEach(t),$t=c(Be),ee=s(Be,"P",{class:!0});var Ra=n(ee);Lt=l(Ra,"September 2022 - May 2023"),Ra.forEach(t),Be.forEach(t),Nt=c(Je),Pe=s(Je,"P",{});var Wa=n(Pe);Dt=l(Wa,`Part of the core development team. Worked on
+                                network-oriented APIs and all the shenanigans
+                                that make it work, like asynchronous task
+                                workers, message queues, and data storage.
+                                Additionally, I reworked the CI / CD flows and
+                                the image build and deployment processes.
+                                Provided customer-centric support with any
+                                development-related issues.`),Wa.forEach(t),Je.forEach(t),za.forEach(t),We.forEach(t),Ft=c(b),$=s(b,"SECTION",{class:!0});var Ye=n($);Se=s(Ye,"H3",{});var Ja=n(Se);Ot=l(Ja,"SilentForce"),Ja.forEach(t),Tt=c(Ye),ke=s(Ye,"UL",{});var Ba=n(ke);R=s(Ba,"LI",{});var Ge=n(R);L=s(Ge,"SPAN",{class:!0});var Ze=n(L);te=s(Ze,"H4",{class:!0});var Ya=n(te);xt=l(Ya,"Security Engineering Intern"),Ya.forEach(t),jt=c(Ze),ae=s(Ze,"P",{class:!0});var Ga=n(ae);Vt=l(Ga,"December 2021 - June 2022"),Ga.forEach(t),Ze.forEach(t),Mt=c(Ge),qe=s(Ge,"P",{});var Za=n(qe);zt=l(Za,`Performed offensive security testing of web and
+                                mobile applications. Helped with the development
+                                of a tool that allowed continuous monitoring,
+                                discovery and testing of customer assets in an
+                                automated fashion. Other notable projects are
+                                the development of an API that exposed all
+                                gathered data to the customers and an
+                                asynchronous web spider that crawled customer
+                                web services in search of sensitive data that
+                                could be used by attackers.`),Za.forEach(t),Ge.forEach(t),Ba.forEach(t),Ye.forEach(t),Ut=c(b),N=s(b,"SECTION",{class:!0});var Ke=n(N);Ae=s(Ke,"H3",{});var Ka=n(Ae);Rt=l(Ka,"Sanic Framework (open-source, volunteering)"),Ka.forEach(t),Wt=c(Ke),W=s(Ke,"UL",{});var Qe=n(W);J=s(Qe,"LI",{});var Xe=n(J);D=s(Xe,"SPAN",{class:!0});var et=n(D);se=s(et,"H4",{class:!0});var Qa=n(se);Jt=l(Qa,"Steering Council"),Qa.forEach(t),Bt=c(et),ne=s(et,"P",{class:!0});var Xa=n(ne);Yt=l(Xa,"October 2022 - Present"),Xa.forEach(t),et.forEach(t),Gt=c(Xe),He=s(Xe,"P",{});var es=n(He);Zt=l(es,`Ensured the smooth operation of the community
+                                projects by removing impediments, and assisting
+                                members as needed. Voiced any concerns,
+                                suggestions or ideas. Handled RFCs, enforced the
+                                community's code of conduct and administered
+                                community assets.`),es.forEach(t),Xe.forEach(t),Kt=c(Qe),B=s(Qe,"LI",{});var tt=n(B);F=s(tt,"SPAN",{class:!0});var at=n(F);oe=s(at,"H4",{class:!0});var ts=n(oe);Qt=l(ts,"Core Developer"),ts.forEach(t),Xt=c(at),re=s(at,"P",{class:!0});var as=n(re);ea=l(as,"October 2021 - October 2022"),as.forEach(t),at.forEach(t),ta=c(tt),Ce=s(tt,"P",{});var ss=n(Ce);aa=l(ss,`Submitted various contributions to the project,
+                                assisted the community and handled bug reports.
+                                Contributed to various discussions about the
+                                long-term direction of the project.`),ss.forEach(t),tt.forEach(t),Qe.forEach(t),Ke.forEach(t),sa=c(b),O=s(b,"SECTION",{class:!0});var st=n(O);$e=s(st,"H3",{});var ns=n($e);na=l(ns,"VATSIM Network (non-profit, volunteering)"),ns.forEach(t),oa=c(st),Le=s(st,"UL",{});var os=n(Le);Y=s(os,"LI",{});var nt=n(Y);T=s(nt,"SPAN",{class:!0});var ot=n(T);le=s(ot,"H4",{class:!0});var rs=n(le);ra=l(rs,"Software Developer"),rs.forEach(t),la=c(ot),ie=s(ot,"P",{class:!0});var ls=n(ie);ia=l(ls,"March 2019 - September 2021"),ls.forEach(t),ot.forEach(t),ca=c(nt),Ne=s(nt,"P",{});var is=n(Ne);da=l(is,`Contributed to the development of several of
+                                services; most notably to their proprietary
+                                audio simulation codec known as "Audio For
+                                VATSIM (AFV)". This codec is used by all of
+                                their clients and simulates real-life VHF and HF
+                                radio transmissions, decreasing audio quality as
+                                distance between transmitter and receiver
+                                increases in the simulation.`),is.forEach(t),nt.forEach(t),os.forEach(t),st.forEach(t),b.forEach(t),Me.forEach(t),ha=c(ue),x=s(ue,"ARTICLE",{class:!0});var rt=n(x);De=s(rt,"H2",{});var cs=n(De);ua=l(cs,"Education"),cs.forEach(t),ma=c(rt),ce=s(rt,"DIV",{class:!0});var ds=n(ce);j=s(ds,"SECTION",{class:!0});var lt=n(j);Fe=s(lt,"H3",{});var hs=n(Fe);pa=l(hs,`U-tad | Centro Universitario de Tecnología y Arte
+                        Digital`),hs.forEach(t),va=c(lt),Oe=s(lt,"UL",{});var us=n(Oe);G=s(us,"LI",{});var it=n(G);de=s(it,"SPAN",{class:!0});var ms=n(de);he=s(ms,"H4",{class:!0});var ps=n(he);fa=l(ps,`Bachelor in Software Engineering
+                                    (Cybersecurity specialization)`),ps.forEach(t),ms.forEach(t),ga=c(it),Te=s(it,"P",{});var vs=n(Te);ya=l(vs,`Honors in Introduction to Programming I & II,
+                                Introduction to Cybersecurity, Distributed
+                                Systems Programming and Networking subjects.`),vs.forEach(t),it.forEach(t),us.forEach(t),lt.forEach(t),ds.forEach(t),rt.forEach(t),ue.forEach(t),je.forEach(t),this.h()},h(){d(o,"class","content svelte-1qma754"),d(m,"class","svelte-1qma754"),d(K,"class","svelte-1qma754"),d(Q,"class","svelte-1qma754"),d(A,"class","item-details svelte-1qma754"),d(q,"class","item svelte-1qma754"),d(X,"class","svelte-1qma754"),d(ee,"class","svelte-1qma754"),d(C,"class","item-details svelte-1qma754"),d(H,"class","item svelte-1qma754"),d(te,"class","svelte-1qma754"),d(ae,"class","svelte-1qma754"),d(L,"class","item-details svelte-1qma754"),d($,"class","item svelte-1qma754"),d(se,"class","svelte-1qma754"),d(ne,"class","svelte-1qma754"),d(D,"class","item-details svelte-1qma754"),d(oe,"class","svelte-1qma754"),d(re,"class","svelte-1qma754"),d(F,"class","item-details svelte-1qma754"),d(N,"class","item svelte-1qma754"),d(le,"class","svelte-1qma754"),d(ie,"class","svelte-1qma754"),d(T,"class","item-details svelte-1qma754"),d(O,"class","item svelte-1qma754"),d(_,"class","content svelte-1qma754"),d(k,"class","svelte-1qma754"),d(he,"class","svelte-1qma754"),d(de,"class","item-details svelte-1qma754"),d(j,"class","item svelte-1qma754"),d(ce,"class","content svelte-1qma754"),d(x,"class","svelte-1qma754"),d(u,"class","sections svelte-1qma754"),d(h,"class","svelte-1qma754")},m(xe,je){ct(xe,h,je),e(h,u),e(u,m),e(m,p),e(p,v),e(m,E),e(m,o),e(o,g),e(g,w),e(o,M),e(o,f),e(f,y),e(y,I),e(f,Z),e(f,S),e(S,dt),e(f,ht),e(f,ve),e(ve,ut),e(o,mt),e(o,fe),e(fe,pt),e(o,vt),e(o,ge),e(ge,ft),e(u,gt),e(u,k),e(k,ye),e(ye,yt),e(k,_t),e(k,_),e(_,q),e(q,_e),e(_e,Et),e(q,wt),e(q,Ee),e(Ee,z),e(z,A),e(A,K),e(K,bt),e(A,It),e(A,Q),e(Q,Pt),e(z,St),e(z,we),e(we,kt),e(_,qt),e(_,H),e(H,be),e(be,At),e(H,Ht),e(H,Ie),e(Ie,U),e(U,C),e(C,X),e(X,Ct),e(C,$t),e(C,ee),e(ee,Lt),e(U,Nt),e(U,Pe),e(Pe,Dt),e(_,Ft),e(_,$),e($,Se),e(Se,Ot),e($,Tt),e($,ke),e(ke,R),e(R,L),e(L,te),e(te,xt),e(L,jt),e(L,ae),e(ae,Vt),e(R,Mt),e(R,qe),e(qe,zt),e(_,Ut),e(_,N),e(N,Ae),e(Ae,Rt),e(N,Wt),e(N,W),e(W,J),e(J,D),e(D,se),e(se,Jt),e(D,Bt),e(D,ne),e(ne,Yt),e(J,Gt),e(J,He),e(He,Zt),e(W,Kt),e(W,B),e(B,F),e(F,oe),e(oe,Qt),e(F,Xt),e(F,re),e(re,ea),e(B,ta),e(B,Ce),e(Ce,aa),e(_,sa),e(_,O),e(O,$e),e($e,na),e(O,oa),e(O,Le),e(Le,Y),e(Y,T),e(T,le),e(le,ra),e(T,la),e(T,ie),e(ie,ia),e(Y,ca),e(Y,Ne),e(Ne,da),e(u,ha),e(u,x),e(x,De),e(De,ua),e(x,ma),e(x,ce),e(ce,j),e(j,Fe),e(Fe,pa),e(j,va),e(j,Oe),e(Oe,G),e(G,de),e(de,he),e(he,fa),e(G,ga),e(G,Te),e(Te,ya)},p:pe,i:pe,o:pe,d(xe){xe&&t(h)}}}class Es extends Sa{constructor(h){super(),ka(this,h,null,_s,qa,{})}}function ws(P){let h,u,m,p,v,E,o,g,w,M;return{c(){h=a("article"),u=a("h1"),m=r(P[0]),p=r(", my name is "),v=a("b"),E=r("Néstor Pérez"),o=r("."),g=i(),w=a("p"),M=r(`I'm a backend software engineer who specializes in asynchronous Python
+        code.`),this.h()},l(f){h=s(f,"ARTICLE",{class:!0});var y=n(h);u=s(y,"H1",{class:!0});var I=n(u);m=l(I,P[0]),p=l(I,", my name is "),v=s(I,"B",{});var Z=n(v);E=l(Z,"Néstor Pérez"),Z.forEach(t),o=l(I,"."),I.forEach(t),g=c(y),w=s(y,"P",{class:!0});var S=n(w);M=l(S,`I'm a backend software engineer who specializes in asynchronous Python
+        code.`),S.forEach(t),y.forEach(t),this.h()},h(){d(u,"class","svelte-z1wgt0"),d(w,"class","svelte-z1wgt0"),d(h,"class","svelte-z1wgt0")},m(f,y){ct(f,h,y),e(h,u),e(u,m),e(u,p),e(u,v),e(v,E),e(u,o),e(h,g),e(h,w),e(w,M)},p(f,[y]){y&1&&fs(m,f[0])},i:pe,o:pe,d(f){f&&t(h)}}}function bs(P,h,u){const m=["Hello","Hola","Bonjour","Hallo","Ciao","Olá"];let p="Hello";return gs(()=>{u(0,p=m[Math.floor(Math.random()*m.length)])}),[p]}class Is extends Sa{constructor(h){super(),ka(this,h,bs,ws,qa,{})}}function Ps(P){let h,u,m,p,v,E;return h=new ys({props:{title:"Néstor Pérez - Software Engineer",description:"Welcome to my website"}}),m=new Is({}),v=new Es({}),{c(){_a(h.$$.fragment),u=i(),_a(m.$$.fragment),p=i(),_a(v.$$.fragment)},l(o){Ea(h.$$.fragment,o),u=c(o),Ea(m.$$.fragment,o),p=c(o),Ea(v.$$.fragment,o)},m(o,g){wa(h,o,g),ct(o,u,g),wa(m,o,g),ct(o,p,g),wa(v,o,g),E=!0},p:pe,i(o){E||(ba(h.$$.fragment,o),ba(m.$$.fragment,o),ba(v.$$.fragment,o),E=!0)},o(o){Ia(h.$$.fragment,o),Ia(m.$$.fragment,o),Ia(v.$$.fragment,o),E=!1},d(o){Pa(h,o),o&&t(u),Pa(m,o),o&&t(p),Pa(v,o)}}}class qs extends Sa{constructor(h){super(),ka(this,h,null,Ps,qa,{})}}export{qs as component};
